8cc8bcf953e96038ab13eadeb51eedecdec8bbad
jeremy1139 <56091873+jeremy1139@users.noreply.github.com>
2021-03-30 13:01:59 -0700

    Drawing.java

---
+++ Drawing.java
@@ -0,0 +1,9 @@
+package revature;
+
+public class Drawing 
+{
+	public static void main(String[] args)
+	{
+		System.out.println(String.format("%1$s \n%2$s \n%3$d \n%4$f \n%5$b", "  /\\>\n /||\\\n/||||\\\n W  W", "Weight/Height/Mammal", 90, 2.5, false));
+	}
+}
